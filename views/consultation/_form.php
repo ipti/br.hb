29e808988cce018b7b351c64128d519af50ce159
franciscocabral <franciscocabralfilho@gmail.com>
2015-03-11 09:15:16 -0300

    Modificação na estrutura dos forms. Utilizando ActiveForm do Kartik-v e Select2

--- views/consultation/_form.php
+++ views/consultation/_form.php
@@ -1,32 +1,52 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use \yii\helpers\ArrayHelper;
+use kartik\widgets\ActiveForm;
+use kartik\builder\Form;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\consultation */
 /* @var $form yii\widgets\ActiveForm */
+/* @var $campaign app\models\campaign */
 ?>
 
 <div class="consultation-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'term')
-            ->dropDownList(\yii\helpers\ArrayHelper::map(
-                    $campaign->getHemoglobins()->where('sample = 1')->all(), 'agreed_term', 'agreedTerm.students.name'),
-                    [$model->isNewRecord ? "":"disabled"=>"disabled"]) ?>
-    
-    <?= Html::activeHiddenInput($model, 'doctor') ?>
-    
-    <?= $form->field($model, 'attended')->checkbox() ?>
-
-    <?= $form->field($model, 'delivered')->checkbox() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+    <?php
+    $form = ActiveForm::begin(['type' => ActiveForm::TYPE_VERTICAL]);
+    echo Form::widget([
+        'model' => $model,
+        'form' => $form,
+        'columns' => 1,
+        'attributes' => [
+            'doctor' => [
+                'type' => Form::INPUT_RAW,
+                'value' => Html::activeHiddenInput($model, 'doctor')
+            ],
+            'term' => [
+                'type' => Form::INPUT_WIDGET,
+                'widgetClass' => Select2::className(),
+                'options' => [
+                    'data' => ArrayHelper::map($campaign->getHemoglobins()->where('sample = 1')->all(), 'agreed_term', 'agreedTerm.students.name'),
+                    'options' => [
+                        'placeholder' => Yii::t('app', 'Select Student...'),
+                        $model->isNewRecord ? "" : "disabled" => "disabled"
+                    ]
+                ],
+            ],
+            'attended' => [
+                'type' => Form::INPUT_CHECKBOX,
+            ],
+            'delivered' => [
+                'type' => Form::INPUT_CHECKBOX,
+            ],
+        ]
+    ]);
+    Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), 
+            ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
+    ActiveForm::end();
+    ?>
 
 </div>
