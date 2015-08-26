a80f12345cd743fb969234665006d1b578cf2bcf
franciscocabral <franciscocabralfilho@gmail.com>
2015-08-26 11:45:55 -0300

    inconsistencia no número de consultas.

--- models/enrollmentSearch.php
+++ models/enrollmentSearch.php
@@ -10,45 +10,45 @@ use app\models\enrollment;
 /**
  * enrollmentSearch represents the model behind the search form about `app\models\enrollment`.
  */
-class enrollmentSearch extends enrollment
-{
-    public function rules()
-    {
+class enrollmentSearch extends enrollment {
+
+    public function rules() {
         return [
             [['id'], 'integer'],
             [['student', 'classroom'], 'safe']
         ];
     }
 
-    public function scenarios()
-    {
+    public function scenarios() {
         // bypass scenarios() implementation in the parent class
         return Model::scenarios();
     }
 
-    public function search($params){
+    public function search($params) {
+        /* @var $query \yii\db\ActiveQuery */
         $actualController = Yii::$app->controller->className();
         $cid = isset($params["c"]) ? $params["c"] : $params["cid"];
-        
-        $campaign = \app\models\campaign::find()->where("id = :cid",["cid"=>$cid])->one();
-    
+
+        $campaign = \app\models\campaign::find()->where("id = :cid", ["cid" => $cid])->one();
+
         $query = $campaign->getEnrollments();
+        $query->distinct = true;
         $query->select("enrollment.*");
-        
-        if($actualController == \app\controllers\ConsultationController::className()){
+
+        if ($actualController == \app\controllers\ConsultationController::className()) {
             $query->innerJoin('term as t', 't.enrollment = enrollment.id and t.agreed = true');
             $query->innerJoin('consultation as co', 'co.term = t.id');
             $query->where("campaign = :cid", ["cid" => $cid]);
         }
-        
+
         $dataProvider = new ActiveDataProvider(['query' => $query,]);
-        $sName ="";
-        $cName ="";
+        $sName = "";
+        $cName = "";
         $query->innerJoin('student as s', 's.id = enrollment.student');
         $query->innerJoin('classroom as c', 'c.id = enrollment.classroom');
-        $query->orderBy('s.name ASC,  c.name ASC') ;
-        
-        if(isset($params["enrollmentSearch"])){
+        $query->orderBy('s.name ASC,  c.name ASC');
+
+        if (isset($params["enrollmentSearch"])) {
             $sName = $params["enrollmentSearch"]['student'];
             $cName = $params["enrollmentSearch"]['classroom'];
             $params["enrollmentSearch"]['student'] = "";
@@ -60,7 +60,8 @@ class enrollmentSearch extends enrollment
 
         $query->andFilterWhere(['id' => $this->id]);
         $query->andFilterWhere(['like', 's.name', $sName])
-              ->andFilterWhere(['like', 'c.name', $cName]);
+                ->andFilterWhere(['like', 'c.name', $cName]);
         return $dataProvider;
     }
+
 }
