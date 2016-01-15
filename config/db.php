4d2a7435251a5c6cac10e1682fc362aba5d14523
franciscocabral <franciscocabralfilho@gmail.com>
2016-01-15 11:25:46 -0300

    Configurações básicas

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db.ipti.org.br;dbname=br.org.ipti.boquim.hb',
+    'dsn' => 'mysql:host=db.ipti.org.br;dbname=br.org.ipti.hb',
     'username' => 'user.hb',
     'password' => '123456',
     'charset' => 'utf8',
