abb3307e2e50bf13155b10029177a6befa4c12f7
RUANCELI SANTOS <ruan@celi.se>
2020-03-02 23:41:30 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.25.114;dbname=br.org.ipti.boquim.hb',
-    'username' => 'user.hb',
+    'dsn' => 'mysql:host=mariadb-s6vhx-mariadb.mariadb-s6vhx.svc.cluster.local;dbname=br.org.ipti.boquim.hb',
+    'username' => 'admin',
     'password' => '123456',
     'charset' => 'utf8',
 ];
