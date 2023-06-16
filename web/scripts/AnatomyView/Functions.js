6e9320c9bdc26085ec03522d88dcb2f6923db11e
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-06-16 11:22:29 -0300

    fix: anatomy view

--- web/scripts/AnatomyView/Functions.js
+++ web/scripts/AnatomyView/Functions.js
@@ -5,3 +5,10 @@ function submitAnatomyForm($form) {
         )
         $('#anatomyModal').modal('hide');
 };
+
+// função para remover um texto perdido inserido no html
+$(document).ready(function() {
+    $('.container').contents().filter(function() {
+        return this.nodeType === 3;
+    }).remove();
+});
