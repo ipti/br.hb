cad36bf2fb57ab86f53e4867b2bd51746280002d
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-06-16 10:50:46 -0300

    fix: sql ferritin

--- banco/migrations/add_ferritin_table.sql
+++ banco/migrations/add_ferritin_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `ferritin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `agreed_term` int(11) NOT NULL,
   `rate` float NOT NULL,
+  `date` DATE NULL,
   PRIMARY KEY (`id`),
   KEY `fk_ferritina_aluno_has_campanha1_idx` (`agreed_term`),
   CONSTRAINT `fk_ferritin_term` FOREIGN KEY (`agreed_term`) REFERENCES `term` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
