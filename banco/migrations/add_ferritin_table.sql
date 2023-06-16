882af21204e765dd542fb32b71ce70ba5f110cda
Carlos Henrique Santos Silveira <109424074+CarlosDcomp@users.noreply.github.com>
2023-06-16 10:46:54 -0300

    feature: DDL ferritin

---
+++ banco/migrations/add_ferritin_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `ferritin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `agreed_term` int(11) NOT NULL,
+  `rate` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ferritina_aluno_has_campanha1_idx` (`agreed_term`),
+  CONSTRAINT `fk_ferritin_term` FOREIGN KEY (`agreed_term`) REFERENCES `term` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2129 DEFAULT CHARSET=utf8;
